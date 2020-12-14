b2b7cdc2c706debcbd7e6b81f1059f2ec55bbe3a
Allen Körm <49126209+AlaenKermit@users.noreply.github.com>
2020-12-14 13:53:22 +0200

    main.py faili tegemine

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import time
+print("""
+.▄▄ ·  ▄▄▄·  ▄▄▄· ▄▄▄  ▄▄▄ .• ▌ ▄ ·.  ▄▄▄·  ▄▄▄·     ▄▄▌ ▐ ▄▌ ▄▄▄· ▄▄▄  .▄▄ · 
+▐█ ▀. ▐█ ▀█ ▐█ ▀█ ▀▄ █·▀▄.▀··██ ▐███▪▐█ ▀█ ▐█ ▀█     ██· █▌▐█▐█ ▀█ ▀▄ █·▐█ ▀. 
+▄▀▀▀█▄▄█▀▀█ ▄█▀▀█ ▐▀▀▄ ▐▀▀▪▄▐█ ▌▐▌▐█·▄█▀▀█ ▄█▀▀█     ██▪▐█▐▐▌▄█▀▀█ ▐▀▀▄ ▄▀▀▀█▄
+▐█▄▪▐█▐█ ▪▐▌▐█ ▪▐▌▐█•█▌▐█▄▄▌██ ██▌▐█▌▐█ ▪▐▌▐█ ▪▐▌    ▐█▌██▐█▌▐█ ▪▐▌▐█•█▌▐█▄▪▐█
+ ▀▀▀▀  ▀  ▀  ▀  ▀ .▀  ▀ ▀▀▀ ▀▀  █▪▀▀▀ ▀  ▀  ▀  ▀      ▀▀▀▀ ▀▪ ▀  ▀ .▀  ▀ ▀▀▀▀ 
+ver 0.1                                              Allan Kerme;Kristjan Kuus
+""")
+time.sleep(3)
+tegelasenimi = str(input("Sisestage oma tegelase nimi: "))
+#print("Kas olete kindel, et soovite oma tegelas")
+print("Palun valige oma tegelasklass:\nRÜÜTEL | VIBUKÜTT | MAAGIAMEISTER")
+tegelasklass = str(input("Tegelasklass: "))
