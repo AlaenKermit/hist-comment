0ffb4a5fca25e9123a67ff11e26e4dd362edaa94
Allen Körm <49126209+AlaenKermit@users.noreply.github.com>
2020-12-14 14:01:38 +0200

    main.py paremaks vormistamine
    samuti sai lisatud kontrollküsimuse lisamine peale tegelasnime valimist

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ print("""
  ▀▀▀▀  ▀  ▀  ▀  ▀ .▀  ▀ ▀▀▀ ▀▀  █▪▀▀▀ ▀  ▀  ▀  ▀      ▀▀▀▀ ▀▪ ▀  ▀ .▀  ▀ ▀▀▀▀ 
 ver 0.1                                              Allan Kerme;Kristjan Kuus
 """)
+
 time.sleep(3)
+
 tegelasenimi = str(input("Sisestage oma tegelase nimi: "))
-#print("Kas olete kindel, et soovite oma tegelas")
+print("Kas olete kindel, et soovite oma tegelase nimeks panna", (tegelasenimi), "?")
+
+kindel = str(input("jah või ei:? "))
+
+if kindel == str("ei"):
+    tegelasenimi = str(input("Sisestage oma tegelase nimi: "))
+    print("Tee olete muudnud oma tegelase nime,", tegelasenimi)
+    
 print("Palun valige oma tegelasklass:\nRÜÜTEL | VIBUKÜTT | MAAGIAMEISTER")
 tegelasklass = str(input("Tegelasklass: "))
