18717a9846473c63fa3978d5ac71084cc1725da0
Allen Körm <49126209+AlaenKermit@users.noreply.github.com>
2021-01-14 15:31:36 +0200

    updated

--- main.py
+++ main.py
@@ -2,18 +2,38 @@ import os
 import time
 def waittimer(x):
     time.sleep(x)
+    
 print("""
 Linux käskude ajaloofaili automatiseeritud kommenteerija
 ver 0.1 // Allan Kerme;Kristjan Kuus
 """)
-waittimer(3)
-failinimi = str(input("""
+
+#waittimer(3)
+
+käsufail= str(input("""
 NB: Faili formaat peab olema
 käsk - ### kommentaar
 Sisestage failinimi käskudega: """))
-historyfail = str(input("Sisestage oma history failinimi, mille käsud soovite kommenteerida"))
+
+historyfail = str(input("Sisestage oma history failinimi, mille käsud soovite kommenteerida: "))
+
 historyfailavatud = open(historyfail, encoding="UTF-8")
-failseletustega = open(failinimi, encoding="UTF-8")
-for i in historyfailavatud:
-    esimenesõna = i.split()[1]
-    print(esimenesõna)
+
+käsufailavatud = open(käsufail, encoding="UTF-8")
+
+historyreadlines = historyfailavatud.readlines()
+käsufailreadlines = käsufailavatud.readlines()
+
+with open("komenteeritudajalugu.txt", "w") as outfile:
+    for hist1 in historyreadlines:
+        hist123 = hist1.strip().rstrip().strip("\n")
+        for käsk1 in käsufailreadlines:
+            if  hist123[1] ==  len(käsk1[0]):
+               outfile.write(hist123 + käsk1)
+            else:
+                print(hist123 + "   ei klapi   " + käsk1)
+                
+
+#for i in historyfailavatud:
+#esimenesõna = i.split()[1]
+#print(esimenesõna)
